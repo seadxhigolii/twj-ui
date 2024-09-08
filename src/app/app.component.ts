8b811252e1a3c5940df12a00fa7cac0542718355
Sead Xhigoli <sead.xhigoli@outlook.com>
2024-09-08 11:52:38 +0200

    Google analytics added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
+declare var gtag: Function;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'argon-dashboard-angular';
+
+export class AppComponent implements OnInit{
+  title = 'argon-dashboard-angular';  
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      gtag('config', 'G-4L0VM03JGW', { 'page_path': event.urlAfterRedirects });
+    });
+  }
 }
